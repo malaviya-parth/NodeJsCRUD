21a1c99d7b294aca77a7f3d4b78006d70ca23a69
Parth <82119640+malaviya-parth@users.noreply.github.com>
2023-05-24 15:19:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import NoteState from './context/notes/NoteState';
+import Alert from './Components/Alert';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import { useState } from 'react';
 
 function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* Write inside Note state so that the context is available to all the children */}
+    <NoteState>
+      <Router>
+        <Navbar/>
+        <Alert alert={alert}/>
+        {/* All the paths are provided inside routes */}
+        <Routes>
+          <Route exact path="/home" element={<Home showAlert={showAlert}/>}>
+          </Route>
+          <Route exact path="/about" element={<About/>}>
+          </Route> 
+          <Route exact path="/login" element={<Login showAlert={showAlert}/>}>
+          </Route> 
+          <Route exact path="/signup" element={<SignUp showAlert={showAlert}/>}>
+          </Route> 
+        </Routes>
+      </Router>
+      </NoteState>
+    </>
+  );  
 }
 
 export default App;
